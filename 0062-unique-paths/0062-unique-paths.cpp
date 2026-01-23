37add26f59e84e662cb2dd2fee66a146549ddc45
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-01-23 11:50:50 +0530

    Time: 2 ms (19.79%), Space: 9.4 MB (51.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int find(int row, int col){
+    int find(int row, int col, vector<vector<int>> &dp){
         if(row==0 || col==0){
-            return 1;
+            return dp[row][col] = 1;
         }else{
-            return find(row-1,col) + find(row, col-1);
+            if(dp[row][col]==0){
+                return dp[row][col] = find(row-1,col,dp) + find(row, col-1,dp);
+            }else{
+                return dp[row][col];
+            }
         }
     }
     int uniquePaths(int m, int n) {
         if(m==1||n==1) return 1;
         int count = 0;
-        
-        return find(m-1,n-1);
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        return find(m-1,n-1,dp);
     }
 };
