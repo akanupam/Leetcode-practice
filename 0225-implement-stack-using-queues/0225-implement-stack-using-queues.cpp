52d9877a5cac2f2f984fe7b09d6e7211f35c44ab
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-02-13 10:58:28 +0530

    Time: 0 ms (100%), Space: 9.4 MB (63.3%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,43 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+    }
+    
+    int pop() {
+        queue<int> temp;
+        while(q.size() > 1){
+            temp.push(q.front());
+            q.pop();
+        }
+        int p = q.front();
+        q.pop();
+        while(!temp.empty()){
+            q.push(temp.front());
+            temp.pop();
+        }
+        return p;
+    }
+    
+    int top() {
+        return q.back();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
