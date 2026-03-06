f06207af5523cc2d8332810aa0a38b40aaf444e5
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-06 14:24:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int f(vector<int>& nums, int k, int i, vector<vector<int>>& dp){
-        // if(k==0){
-        //     return 1;
-        // }
-        // if(i==0){
-        //     if(nums[i]==k) return 1;
-        //     else return 0;
-        // }
         if(i==0){
             if(k==0 && nums[0]==0) return 2;
             else if(k==0 || nums[0]==k) return 1;
@@ -25,11 +18,30 @@ public:
         int n = nums.size();
         int totSum = 0;
         for(int i=0;i<n ;i++) totSum += nums[i];
-        if(totSum<target) return 0;
-        if((totSum-target)%2!=0) return 0;
-        int k = (totSum-target)/2; 
-        vector<vector<int>> dp(n, vector<int>(k+1,-1));
-        int ans = f(nums, k, n-1, dp);
+        if(totSum<abs(target)) return 0;
+        if((totSum+target)%2!=0) return 0;
+        int k = (totSum+target)/2; 
+        vector<int> dp(k+1,0);
+        if(nums[0]==0){
+            dp[0] = 2;
+        }
+        else {
+            dp[0] = 1;
+            if(nums[0]<=k) dp[nums[0]] = 1;
+        }
+        for(int i=1;i<n;i++){
+            vector<int> curr(k+1, 0);
+            for(int j=0;j<=k;j++){
+                int np = dp[j];
+                int p = 0;
+                if(nums[i]<=j){
+                    p = dp[j- nums[i]];
+                }
+                curr[j] = p+np;
+            }
+            dp = curr;
+        }
+        int ans = dp[k];
         return ans;
     }
 };
