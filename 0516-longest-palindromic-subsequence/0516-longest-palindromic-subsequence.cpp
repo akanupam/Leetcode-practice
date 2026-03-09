4906fa59aa41e48cb551901ea92eb2a7e15a3295
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-09 14:33:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -13,20 +13,25 @@ public:
         int n = s.length();
         int left = 0;
         int right = n-1;
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            dp[i][i] = 1;
-        }
+        // vector<vector<int>> dp(n, vector<int>(n,0));
+        vector<int> dp(n,0);
+        // for(int i=0;i<n;i++){
+        //     dp[i][i] = 1;
+        // }
         for(int i=n-1;i>=0;i--){
+            int prev = 0;
+            dp[i] = 1;
             for(int j=i+1;j<n;j++){
+                int temp = dp[j];
                 if(s[i]==s[j]){
-                    dp[i][j] = 2 + dp[i+1][j-1];
+                    dp[j] = 2 + prev;
                 }else{
-                    dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
+                    dp[j] = max(dp[j],dp[j-1]);
                 }
+            prev = temp;
             }
         }
         // return f(left, right, s, dp);
-        return dp[0][n-1];
+        return dp[n-1];
     }
 };
