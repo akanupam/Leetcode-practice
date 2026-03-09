bb080dc899a8a96087998757741e3e20e0933cc6
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-09 15:09:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -13,7 +13,17 @@ public:
         int n = s.length();
         int left = 0;
         int right = n-1;
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return f(left, right, s, dp);
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j]){
+                    dp[i][j] = dp[i+1][j-1];
+                }else{
+                    dp[i][j] = 1 + min(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][n-1];
+        // return f(left, right, s, dp);
     }
 };
