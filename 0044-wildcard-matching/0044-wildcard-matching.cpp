ad17ff923b5fa1434fb7895d1ffb569a94d353ff
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-12 12:27:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -23,21 +23,24 @@ public:
         int n = s.length();
         int m = p.length();
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0] = true;
+        vector<bool> prev(m+1, false);
+        prev[0] = true;
         for(int i=1;i<=m;i++){
             if(p[i-1] == '*')
-                dp[0][i] = dp[0][i-1];
+                prev[i] = prev[i-1];
         }
 
         for(int i=1;i<=n;i++){
+            vector<bool> curr(m+1, false);
             for(int j=1;j<=m;j++){
                 if(s[i-1]==p[j-1]||p[j-1]=='?'){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }else if(p[j-1]=='*'){
-                    dp[i][j] = dp[i-1][j]||dp[i][j-1];
+                    curr[j] = prev[j]||curr[j-1];
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
