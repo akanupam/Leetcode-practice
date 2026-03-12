df92899f4bf88a083aea5f601c12e0fed0c8953e
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-12 11:27:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,23 +14,27 @@ public:
         int n = word1.length();
         int m = word2.length();
         vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+        vector<double> prev(m+1,0);
+        // for(int i=1;i<=n;i++){
+        //     dp[i][0] = i;
+        // }
+        for(int j=0;j<=m;j++){
+            prev[j] = j;
+        }
 
         for(int i=1;i<=n;i++){
-            dp[i][0] = i;
-        }
-        for(int j=0;j<=m;j++){
-            dp[0][j] = j;
-        }
-        for(int i=1;i<=n;i++){
+            vector<double> curr(m+1,0);
+            curr[0] = i;
             for(int j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }else{
-                    dp[i][j] = min(1 + dp[i-1][j], min(1+dp[i-1][j-1], 1+ dp[i][j-1]));
+                    curr[j] = min(1 + prev[j], min(1+prev[j-1], 1+ curr[j-1]));
                 }
             }
+            prev= curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
         // return f(n-1, m-1, word1,word2,dp);
 
     }
