3d272631872b31eaedcc5ffa7035f1b85b039053
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-03-12 10:38:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    int f(int i, int j, string& s, string& t, vector<vector<int>>& dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]= (f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp));
-        }else{
-            return dp[i][j] = f(i-1,j,s,t,dp);
-        }
-    }
+    // int f(int i, int j, string& s, string& t, vector<vector<int>>& dp){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j]){
+    //         return dp[i][j]= (f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp));
+    //     }else{
+    //         return dp[i][j] = f(i-1,j,s,t,dp);
+    //     }
+    // }
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<double>> dp(n+1, vector<double>(m+1,-1));
-        for(int i=1;i<=m;i++){
-            dp[0][i] = 0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+        // vector<vector<double>> dp(n+1, vector<double>(m+1,-1));
+        vector<double> prev (m+1,0);
+        prev[0] = 1;
+        // for(int i=1;i<=m;i++){
+        //     dp[0][i] = 0;
+        // }
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 1;
+        // }
         for(int i=1;i<=n;i++){
+            vector<double> curr(m+1,0);
+            curr[0] = 1;
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
         // return f(n-1,m-1,s,t,dp);
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
