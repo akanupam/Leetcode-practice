0ae7e46f93c94f71bf4959062bbc6f2694de1f00
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-04-07 12:01:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -17,7 +17,19 @@ public:
         sort(cuts.begin(),cuts.end());
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
-        vector<vector<int>> dp(c+1, vector<int>(c+1,-1));
-        return f(1,c,cuts,dp);
+        vector<vector<int>> dp(c+2, vector<int>(c+2,0));
+        // return f(1,c,cuts,dp);
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+                int mini = INT_MAX;
+                for(int k=i;k<=j;k++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j];
+                    mini = min(cost,mini);
+                }
+                dp[i][j]=mini;
+            }
+        }
+    return dp[1][c];
     }
 };
