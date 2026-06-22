f576b2eb883cba7753728a89ff6e616a53bd3b1b
Aditya Kumar Anupam <143707452+akanupam@users.noreply.github.com>
2026-06-22 10:46:35 +0530

    Time: 0 ms (100%), Space: 28.2 MB (63.59%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = nums[0];
+        int count = 0;
+        for(int num:nums){
+            if(count==0){
+                major=num;
+            }
+            if(num==major){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return major;
+    }
+};
